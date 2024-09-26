a1e251aa964e08b98211821661d192ca70f61adb
Kliver59 <christopher59880@hotmail.fr>
2024-09-26 09:41:32 +0200

    commit

---
+++ middlewares/error-handler.js
@@ -0,0 +1,9 @@
+import { StatusCodes } from "http-status-codes";
+
+const errorHandler = (err, req, res, _next) => {
+  console.error(err);
+
+  res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err.message });
+};
+
+export default errorHandler;
